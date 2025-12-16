d0d34a3c91ffc01ba1a39f734a8f1e5554fd8648
DIVYANSHU PRATAP SINGH <divyanshupratapsingh@DIVYANSHUs-MacBook-Air.local>
2025-12-16 22:07:22 +0530

    removed the initial content from readme

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from './middleware';
+import { CreateUserSchema } from "@repo/common/types"
 
 const app = express();
 app.use(express.json());
