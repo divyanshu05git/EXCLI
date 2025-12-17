0170f187b2634b9b3f4b4dbfbf0fe6b91769f5ba
DIVYANSHU PRATAP SINGH <divyanshupratapsingh@DIVYANSHUs-MacBook-Air.local>
2025-12-17 22:21:16 +0530

    websocket backend completed

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -111,6 +111,23 @@ app.post("/room",middleware,async (req,res)=>{
     }
 })
 
+app.get("/chats/:roomId",async(req,res)=>{
+    const roomId=Number(req.params.roomId);
+    const messages=await prismaClient.chat.findMany({
+        where:{
+            roomId:roomId
+        },
+        orderBy:{
+            id:"desc"
+        },
+        take:50
+    })
+
+    res.json({
+        messages
+    })
+})
+
 app.listen(5050, () => {
   console.log('HTTP backend server is running ');
 })
